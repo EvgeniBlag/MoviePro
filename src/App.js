54757eb32e40f7bef77577f5364a63a9360708c4
EvgeniBlag <blagodarni@gmail.com>
2022-03-26 13:21:18 +0100

    probe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
-
+function Food ({name}){
+  return(<h3>I love {name}</h3>)
+}
 
 function App() {
   return (
     <div className="App">
-    
+    <h1>Hello!!</h1>
+    <Food name="burger"/>
+    <Food name="spagetis"/>
+    <Food name="sandwiches"/>
+    <Food name="lasagna"/>
     </div>
   );
 }
